cf76bc9857e5cee99c7bebfa3b0219e5e6555736
Md. Najmul Hasan <najmul.csebuet@gmail.com>
2023-09-21 06:54:14 +0600

    nested routes for comments

--- RubyOnRails/blog/config/routes.rb
+++ RubyOnRails/blog/config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # get "/articles", to: 'articles#index'
   # get "/articles/:id", to: "articles#show"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
 end
