a6487ee883bdb5488e20f5c3a783d7c314c94cb4
Md. Najmul Hasan <najmul.csebuet@gmail.com>
2023-12-04 18:30:40 +0600

    some access rules for Post

--- RubyOnRails/Authorization-Tutorial/Action-Policy-Based/blog_app/app/policies/post_policy.rb
+++ RubyOnRails/Authorization-Tutorial/Action-Policy-Based/blog_app/app/policies/post_policy.rb
@@ -2,4 +2,16 @@ class PostPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  # Quick tip: Action Policy can reference the current_user provided by Devise
+  # and assign it to a user within the policy.
+  def update?
+    # post can only be updated by an author role or the post's author
+    user.author? || (user.id == record.user_id)
+  end
+
+  def destroy
+    # a post can only be deleted by its author
+    user.id == record.user_id
+  end
 end
