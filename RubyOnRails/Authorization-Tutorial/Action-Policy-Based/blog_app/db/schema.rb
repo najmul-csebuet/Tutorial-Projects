a1f98371b892c550522c65d5f72cd57adec73ec6
Md. Najmul Hasan <najmul.csebuet@gmail.com>
2023-12-04 12:17:39 +0600

    rails db:migrate

--- RubyOnRails/Authorization-Tutorial/Action-Policy-Based/blog_app/db/schema.rb
+++ RubyOnRails/Authorization-Tutorial/Action-Policy-Based/blog_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_04_054656) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_061627) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_054656) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
